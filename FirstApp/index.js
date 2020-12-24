2db1775beb3daf9ad94e005f307374f1dc9c4e72
Deepakshi <deepakshisud@gmail.com>
2020-12-24 12:17:27 +0530

    Routing in Express

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -9,11 +9,21 @@ app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000");
 })
 
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`<h1>Browing the ${subreddit} page</h1>`);
+    console.log(req.params);
+})
+
 app.get('/cats', (req,res) => {
     res.send('MEOW!!!')
 });
 
 app.get('/', (req,res) =>{
     res.send("This is homepage");
+})
+
+app.get('*',(req, res) => {
+    res.send("Cannot find the path");
 })
 
