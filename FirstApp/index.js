b15f204d7fc500fbca3ef28e33c080b0a27b0d9e
Deepakshi <deepakshisud@gmail.com>
2020-12-24 12:02:01 +0530

    Express started

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express()
 
-app.use(() => {
-    console.log("WE GOT A NEW REQUESST");
-})
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUESST");
+//     res.send({ color: 'red'})
+// })
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000");
+})
+
+app.get('/cats', (req,res) => {
+    res.send('MEOW!!!')
+});
+
+app.get('/', (req,res) =>{
+    res.send("This is homepage");
 })
 
