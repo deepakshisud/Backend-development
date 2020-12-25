81a1f8bf218c243b50e139631fb06e6c2a31c93e
Deepakshi <deepakshisud@gmail.com>
2020-12-25 11:29:14 +0530

    Bootstrap and Express

--- ejs-app/index.js
+++ ejs-app/index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const app = express()
 const redditData = require('./data.json');
 
+app.use(express.static(path.join(__dirname,'public')))
+
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'/views'));
 
