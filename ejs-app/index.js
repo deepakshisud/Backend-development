01cfae554a7b9c8399bfdf955e9908b3f59dedf6
Deepakshi <deepakshisud@gmail.com>
2020-12-25 10:05:33 +0530

    Ejs templating

--- ejs-app/index.js
+++ ejs-app/index.js
@@ -8,6 +8,11 @@ app.set('views',path.join(__dirname,'/views'));
 app.get('/', (req,res) => {
     res.render('home.ejs');
 })
+
+app.get('/random', (req, res) => {
+    const num = Math.floor(Math.random() *10) + 1;
+    res.render('random',{rand: num})
+})
 app.listen(3000, () => {
     console.log("Listening on Port 3000");
 })
