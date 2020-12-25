be37c3f2d33f25c62f35ddcc2bbe9ecb81e9a125
Deepakshi <deepakshisud@gmail.com>
2020-12-25 10:41:45 +0530

    Basic templatinf with ejs done!

--- ejs-app/index.js
+++ ejs-app/index.js
@@ -18,7 +18,12 @@ app.get('/random', (req, res) => {
 app.get('/r/:subreddit', (req,res) =>{
     const {subreddit} = req.params;
     const data = redditData[subreddit];
+    if(data) {
     res.render('subreddit', {...data});
+    }
+    else {
+        res.render('notfound', { subreddit });
+    }
 })
 
 app.get('/dogs', (req, res) => {
