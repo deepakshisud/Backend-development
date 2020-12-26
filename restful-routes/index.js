108fba1a6f015349a2d9f1e26a0bb1ac2f8a0d0c
Deepakshi <deepakshisud@gmail.com>
2020-12-26 13:55:04 +0530

    Routes defined

--- restful-routes/index.js
+++ restful-routes/index.js
@@ -1,11 +1,12 @@
 // GET /comments - list all comments
-// POST /comments - Create a new comment
+// POST /comments/new - Create a new comment
 // GET /comments/:id - Get one comment (using ID)
 // PATCH /comment/:id - Update one comment
 // DLETE /comment/:id - Destroy one comment
 
 
 const express = require('express');
+const { request } = require('http');
 const app = express();
 const path = require('path');
 
@@ -21,18 +22,22 @@ app.get('/tacos', (req,res) =>  {
 
 const comments = [
     {
+        id: 1,
         username: 'Deepakshi',
         comment: 'Lol thats so funny'
     },
     {
+        id: 2,
         username: 'Aadi',
         comment: 'I love u'
     },
     {
+        id: 3,
         username: 'Maanas',
         comment: 'Lets play something'
     },
     {
+        id: 4,
         username: 'Muma',
         comment: 'Where is Spinny?'
     }
@@ -43,6 +48,12 @@ app.get('/comments', (req,res) => {
     res.render('comments/index.ejs', {comments})
 })
 
+app.get('/comments/:id', (req, res) => {
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === parseInt(id));
+    res.render('comments/show', {comment})
+})
+
 app.get('/comments/new', (req,res) => {
     res.render('comments/new.ejs')
 })
@@ -50,7 +61,7 @@ app.get('/comments/new', (req,res) => {
 app.post('/comments', (req, res) => {
     const {username, comment} = req.body;
     comments.push({username, comment});
-    res.send("It worked")
+    res.redirect('/comments')
 })
 app.post('/tacos', (req,res) => {
     const {meat, quantity} = req.body;
