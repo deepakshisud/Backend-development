6b20d28d8205761ba3ae4b0ced306f0e4d61534f
Deepakshi <deepakshisud@gmail.com>
2021-01-26 15:55:02 +0530

    Mongoose Middleware

--- Mongoose-App/person.js
+++ Mongoose-App/person.js
@@ -17,4 +17,14 @@ personSchema.virtual('fullName').get(function() {
     return `${this.first} ${this.last}`
 })
 
+personSchema.pre('save', async function() {
+    this.first = "Yo";
+    this.last = "MAMA";
+    console.log("About to save");
+})
+
+personSchema.post('save', async function() {
+    console.log("Just saved");
+})
+
 const Person = mongoose.model('Person', personSchema);
