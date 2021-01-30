b0213664af2ef43f29843ab39bfdf6250ec49dcf
Deepakshi <deepakshisud@gmail.com>
2021-01-30 13:38:37 +0530

    Defining own middleware

--- middleware/index.js
+++ middleware/index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 
+app.use((req, res, next) => {
+    console.log("First middleware");
+    return next();
+})
+
+app.use((req, res, next) => {
+    console.log("Second middleware");
+})
 
 app.get('/', (req, res) => {
     res.send("Homepage");
