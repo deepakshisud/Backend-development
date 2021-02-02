8d0e6c33297d9622d77a851f456b3feaf76ae412
Deepakshi <deepakshisud@gmail.com>
2021-02-02 13:57:26 +0530

    Error handling added

--- Mongoose-Express/index.js
+++ Mongoose-Express/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-
+const AppError = require('./AppError');
 const Book = require('./models/books');
 
 mongoose.connect('mongodb://localhost:27017/booksApp', {useNewUrlParser: true, useUnifiedTopology: true})
@@ -27,37 +27,59 @@ app.get('/books', async (req, res) => {
 })
 
 app.get('/books/new', (req, res) => {
+    throw new AppError('NOT ALLOWED', 401);
     res.render('new');
 })
 
 app.get('/books/:id/edit', async (req, res) => {
     const {id} = req.params;
     const foundBook = await Book.findById(id);
+    if(!foundBook) {
+        return next(new AppError('Book not found', 404));
+    }
     res.render('edit', {foundBook});
 })
 
-app.post('/books', async (req, res) => {
-    const newBook = new Book(req.body);
-    await newBook.save();
-    res.redirect(`/books/${newBook._id}`)
+app.post('/books', async (req, res, next) => {
+    try {
+        const newBook = new Book(req.body);
+        await newBook.save();
+        res.redirect(`/books/${newBook._id}`)
+    } catch(e) {
+        next(e);
+    }
+
 })
 
-app.get('/books/:id', async (req, res) => {
+app.get('/books/:id', async (req, res, next) => {
     const {id} = req.params;
     const foundBook = await Book.findById(id);
+    if(!foundBook) {
+        return next(new AppError('Book not found', 404));
+    }
     res.render('show', {foundBook});
 })
 
-app.put('/books/:id', async (req, res) => {
-    const {id} = req.params;
-    const book = await Book.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
-    res.redirect(`/books/${book._id}`)
+app.put('/books/:id', async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const book = await Book.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+        res.redirect(`/books/${book._id}`)
+    } catch(e) {
+        next(e);
+    }
+
 })
 
 app.delete('/books/:id', async (req, res) => {
     const {id} = req.params;
     const deletedBook = await Book.findByIdAndDelete(id);
     res.redirect('/books');
+})
+
+app.use((err,req,res,next) => {
+    const { status = 500, message='Something went wrong'} = err;
+    res.status(status).send(message);
 })
 
 app.listen(3000, () => {
