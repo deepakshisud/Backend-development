a9ae32f231cecf6b7ede995fbd035ae7682b317c
Deepakshi <deepakshisud@gmail.com>
2021-02-02 13:18:09 +0530

    Error handling added

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const AppError = require('./AppError');
 
 // app.use(morgan('tiny'));
 
@@ -21,12 +22,17 @@ const verify_password = (req, res, next) => {
     const {p} = req.query;
     if(p  === 'deepakshi') {
     next(); }
-    res.send("Sorry you need the password");
+    //res.send("Sorry you need the password");
+    throw new AppError("Password required",401);
 }
 
 
 app.get('/', (req, res) => {
     res.send("Homepage");
+})
+
+app.get('/error', (req, res) => {
+    chicken.fly();
 })
 
 app.get('/dogs', (req, res) => {
@@ -40,6 +46,12 @@ app.get('/secret', verify_password, (req, res) => {
 
 app.use((req,res) => {
     res.status(404).send('404 Error');
+})
+
+app.use((err,req,res,next) => {
+    console.log('***************');
+    next(err);
+
 })
 
 app.listen(3000, () => {
