6a6b32dc10f992e6277ddf0788f096f48b116c9b
Deepakshi <deepakshisud@gmail.com>
2021-02-14 15:33:51 +0530

    Login and Register authentication

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -1,14 +1,58 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const app = express();
 const User = require('./models/user');
 
+mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log("Connection open");
+    })
+    .catch(err => {
+        console.log("Error detected");
+        console.log(err);
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send("Home page");
+})
+
 app.get('/register', (req, res) => {
     res.render('register');
+})
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.post('/login', async(req, res) => {
+    const {username, password} = req.body;
+    const user = await User.findOne({username})
+    const validPassword = await bcrypt.compare(password,user.password);
+    if(validPassword) {
+        res.send("Welcome")
+    }
+    else {
+        res.send("try again");
+    }
+})
+
+app.post('/register', async(req, res) => {
+    const {password, username} = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
 })
 
 app.get('/secret', (req, res) => {
